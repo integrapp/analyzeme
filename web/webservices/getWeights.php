c447fd7b63a5e6b1784dd3570fb0fd1a6308cc16
integrapp <eandres@integrapp.es>
2013-06-29 16:45:40 +0200

    debug

--- web/webservices/getWeights.php
+++ web/webservices/getWeights.php
@@ -1,6 +1,7 @@
 <?php
-require('../config/constants.php');
-require('../classes/Database.php');
+error_reporting(E_ALL);
+require('config/constants.php');
+require('classes/Database.php');
 
 if( !empty($_GET) ) {
     foreach($_GET as $key => $value) {
@@ -11,17 +12,17 @@ if( !empty($_GET) ) {
     $db->init(DB_HOST, DB_USER, DB_PSW, DB_NAME);
     $db->connect();
 
-    $sql_Get = sprintf('SELECT * FROM app_promedios WHERE comercial = %s AND 
-        granja = %s ORDER BY (fecha) DESC, id DESC ', $comercial, $granja);
+    $sql_Get = 'SELECT date, quantity FROM activities WHERE user = 1 AND 
+        activity = 3';
 
     $db->query($sql_Get);
     if( $db->countRows() >0 ) {
-        $averages = $db->loadRows(MYSQL_ASSOC);
-        foreach($averages as $int => $value) {
-            list($fecha, $hora) = explode(' ', $value['fecha']);
-            $promediates[$fecha][$value['id']] = date('H:i A', strtotime($value['fecha']));
+        $weights = $db->loadRows(MYSQL_ASSOC);
+        foreach($weights as $int => $weight) {
+            list($fecha, $hora) = explode(' ', $weight['date']);
+            $weights[$fecha] = $weight['quantity'];
         }
-        $data = $promediates;
+        $data = $weights;
     } else {
         $data = '';
     }
